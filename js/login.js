f7c379840eec71856814d291bd9c3a8d590fa8dd
tuliocunha <tulio.cunha@hotmail.com>
2024-11-03 02:24:36 +0000

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,5 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Add login functionality here
+    alert("Login functionality not implemented.");
+});
