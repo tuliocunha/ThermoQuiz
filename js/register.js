d65ff1923dce7999ceef295f0d8f2952deadff98
tuliocunha <tulio.cunha@hotmail.com>
2024-11-03 02:25:16 +0000

    Create register.js

---
+++ js/register.js
@@ -0,0 +1,5 @@
+document.getElementById("registerForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Add registration functionality here
+    alert("Registration functionality not implemented.");
+});
